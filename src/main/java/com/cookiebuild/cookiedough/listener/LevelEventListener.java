0053e8af292394373b1b7b90e3757e77adc8772e
Guillaume Claverie <guillaume.claverie@mail.com>
2024-06-21 19:10:24 +0200

    Disable DayLight cycle for all levels

--- src/main/java/com/cookiebuild/cookiedough/listener/LevelEventListener.java
+++ src/main/java/com/cookiebuild/cookiedough/listener/LevelEventListener.java
@@ -1,8 +1,10 @@
 package com.cookiebuild.cookiedough.listener;
 
+import org.bukkit.GameRule;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.event.world.WorldLoadEvent;
 
 public class LevelEventListener implements Listener {
 
@@ -11,4 +13,8 @@ public class LevelEventListener implements Listener {
         event.setCancelled(true);
     }
 
+    @EventHandler
+    public void onLevelLoad(WorldLoadEvent event) {
+        event.getWorld().setGameRule(GameRule.DO_DAYLIGHT_CYCLE, Boolean.FALSE);
+    }
 }
