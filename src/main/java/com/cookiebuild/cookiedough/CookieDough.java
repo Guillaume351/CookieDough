eb518ffd4f75d6d494fa7af97b6ce6c000ec36ca
Guillaume Claverie <guillaume.claverie@mail.com>
2024-07-22 16:32:28 +0200

    feat: enable Chat Manager

--- src/main/java/com/cookiebuild/cookiedough/CookieDough.java
+++ src/main/java/com/cookiebuild/cookiedough/CookieDough.java
@@ -1,7 +1,9 @@
 package com.cookiebuild.cookiedough;
 
-import com.cookiebuild.cookiedough.listener.WorldEventListener;
+import com.cookiebuild.cookiedough.chat.ChatManager;
+import com.cookiebuild.cookiedough.listener.PlayerChatListener;
 import com.cookiebuild.cookiedough.listener.PlayerWrapperListener;
+import com.cookiebuild.cookiedough.listener.WorldEventListener;
 import com.cookiebuild.cookiedough.utils.HibernateUtil;
 import com.cookiebuild.cookiedough.utils.RabbitMQInitializer;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,16 +20,25 @@ public final class CookieDough extends JavaPlugin {
     public void registerListeners() {
         getServer().getPluginManager().registerEvents(new PlayerWrapperListener(), this);
         getServer().getPluginManager().registerEvents(new WorldEventListener(), this);
+
+        ChatManager chatManager = new ChatManager();
+        getServer().getPluginManager().registerEvents(new PlayerChatListener(chatManager), this);
     }
 
     @Override
     public void onEnable() {
         this.getLogger().info("Enabling CookieDough");
+        // Affect instance
+        instance = this;
+
         // Plugin startup logic
         sessionFactory = HibernateUtil.buildSessionFactory();
 
         // Initialize RabbitMQ
         RabbitMQInitializer.initialize();
+
+        // register listeners
+        registerListeners();
 
         this.getLogger().info("CookieDough enabled!");
     }
--- src/main/java/com/cookiebuild/cookiedough/listener/PlayerChatListener.java
+++ src/main/java/com/cookiebuild/cookiedough/listener/PlayerChatListener.java
@@ -6,7 +6,6 @@ import io.papermc.paper.event.player.AsyncChatEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatListener implements Listener {
 
