250f6419edcf9db7f48b61338285bace79ae00ae
Guillaume Claverie <guillaume.claverie@mail.com>
2024-07-23 18:42:34 +0200

    feat: player join message

--- src/main/java/com/cookiebuild/cookiedough/listener/PlayerWrapperListener.java
+++ src/main/java/com/cookiebuild/cookiedough/listener/PlayerWrapperListener.java
@@ -2,8 +2,10 @@ package com.cookiebuild.cookiedough.listener;
 
 import com.cookiebuild.cookiedough.player.CookiePlayer;
 import com.cookiebuild.cookiedough.player.PlayerManager;
+import com.cookiebuild.cookiedough.utils.LocaleManager;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,12 +15,22 @@ public class PlayerWrapperListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        // TODO
+        // Remove default join message
+        event.setJoinMessage(null);
+
         Player player = event.getPlayer();
 
         new CookiePlayer(player);
-    }
 
+        // Send welcome message
+        player.sendMessage(ChatColor.GREEN + LocaleManager.getMessage("welcome.message", player.locale(), player.getName()));
+
+        // Show it as a title
+        player.sendTitle(ChatColor.translateAlternateColorCodes('&', LocaleManager.getMessage("welcome.message", player.locale(), player.getName())),
+                "",
+                10, 60, 10);
+        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+    }
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         // TODO
