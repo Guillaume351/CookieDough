eee7f1ef1c0b968b5763b71481b38be861b3463e
Guillaume Claverie <guillaume.claverie@mail.com>
2026-07-13 21:35:56 +0200

    fix: restore Bedrock NPC hits

--- src/main/java/com/cookiebuild/cookiedough/lobby/NPCListener.java
+++ src/main/java/com/cookiebuild/cookiedough/lobby/NPCListener.java
@@ -1,11 +1,13 @@
 package com.cookiebuild.cookiedough.lobby;
 
+import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -14,7 +16,6 @@ import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class NPCListener implements Listener {
-
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
         handleNpcInteraction(event.getPlayer(), event.getRightClicked(), event);
@@ -23,6 +24,21 @@ public class NPCListener implements Listener {
     @EventHandler
     public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
         handleNpcInteraction(event.getPlayer(), event.getRightClicked(), event);
+    }
+
+    /**
+     * Paper 26 fires this before checking whether the attacked entity can take
+     * damage. Geyser translates a Bedrock entity hit to the same Java attack
+     * packet, so this still fires for our invulnerable zombie NPCs even when an
+     * EntityDamageByEntityEvent is never produced.
+     */
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerAttackEntity(PrePlayerAttackEntityEvent event) {
+        GameNPC npc = resolveNpc(event.getAttacked());
+        if (npc != null) {
+            event.setCancelled(true);
+            npc.interactWithPlayer(event.getPlayer());
+        }
     }
 
     @EventHandler
